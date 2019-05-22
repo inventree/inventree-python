e8d77d2f3b655fb66a82cc913a373265b0cef646
Oliver Walters <oliver.henry.walters@gmail.com>
2019-05-23 00:32:45 +1000

    Add some new classes
    - Part
    - SupplierPart
    - SupplierPriceBreak

--- inventree_object.py
+++ inventree_object.py
@@ -4,18 +4,25 @@ import json
 class InventreeObject():
     """ Base class for an InvenTree object """
 
-    def __init__(self, url, requester, raw_data={}):
+    URL = ""
+
+    def __init__(self, requester, pk=None, data={}):
         """ Insantiate this InvenTree object.
 
         Args:
-            url - The URL associated with this object on the server
+            pk - The ID (primary key) associated with this object on the server
             requester - The request manager object
             data - JSON representation of the object
         """
 
-        self._url = url
+        # If the pk is not explicitly provided,
+        # extract it from the provided dataset
+        if pk is None:
+            pk = data['pk']
+
+        self._url = "{url}/{pk}/".format(url=self.URL, pk=pk)
         self._requester = requester
-        self._data = raw_data
+        self._data = data
 
         # If the data are not populated, fetch from server
         if len(self._data) == 0:
@@ -50,3 +57,52 @@ class InventreeObject():
             self._data[name] = value
         else:
             raise KeyError("Key '{k}' does not exist in dataset".format(k=name))
+
+
+class Part(InventreeObject):
+    """ Class for manipulating a Part object """
+
+    URL = 'part'
+
+    def get_supplier_parts(self):
+
+        response = self._requester.get(
+            SupplierPart.URL,
+            params={
+                'part': self['pk']
+            })
+
+        parts = []
+
+        for part in response:
+            if 'pk' in part.keys():
+                parts.append(SupplierPart(data=part, requester=self._requester))
+
+        return parts
+
+    
+class SupplierPart(InventreeObject):
+    """ Class for maniuplating a SupplierPart object """
+
+    URL = 'company/part'
+
+    def get_price_breaks(self):
+
+        response = self._requester.get(
+            SupplierPriceBreak.URL,
+            params={
+                'part': self['pk']
+            })
+
+        breaks = []
+
+        for brk in response:
+            if 'pk' in brk.keys():
+                breaks.append(SupplierPriceBreak(self._requester, data=brk))
+
+        return breaks
+
+
+class SupplierPriceBreak(InventreeObject):
+
+    URL = 'company/price-break/'
