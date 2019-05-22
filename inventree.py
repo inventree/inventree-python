0263c19794742602d1eafa8ce5824b72601f2591
Oliver Walters <oliver.henry.walters@gmail.com>
2019-05-22 22:29:47 +1000

    Request method for managing low level requests

--- inventree.py
+++ inventree.py
@@ -2,9 +2,15 @@ import requests
 from requests.auth import HTTPBasicAuth
 import os
 import json
+import logging
 
 
-class InventreeRequest(object):
+class InventreeRequester(object):
+    """ Basic class for performing Inventree API requests.
+
+    GET - Fetch data from the server
+
+    """ 
 
     def __init__(self, base_url, **kwargs):
         """ Initialize class with initial parameters
@@ -28,6 +34,35 @@ class InventreeRequest(object):
         else:
             self.auth = None
 
+    def request(self, url, **kwargs):
+        """ Perform a URL request to the Inventree API """
+
+        api_url = os.path.join(self.base_url, url)
+
+        method = kwargs.get('method', 'get')
+
+        methods = {
+            'GET': requests.get,
+            'POST': requests.post,
+            'DELETE': requests.delete,
+        }
+
+        if method.upper() not in methods.keys():
+            logging.error("Unknown request method '{m}'".format(m=method))
+            return None
+
+        method = method.upper()
+
+        try:
+            response = methods[method](api_url, auth=self.auth)
+        except requests.exceptions.ConnectionError:
+            logging.error('Connection refused - {url}'.format(url=api_url))
+            return None
+            
+        logging.info("Request: {method} {url} - {response}".format(method=method, url=api_url, response=response.status_code))
+
+        return response
+
     def get(self, url, **kwargs):
         """ Perform a GET request
 
@@ -38,9 +73,7 @@ class InventreeRequest(object):
 
         """
 
-        print(os.path.join(self.base_url, url))
-
-        response = requests.get(os.path.join(self.base_url, url), auth=self.auth)
+        response = self.request(url, method='get')
 
         try:
             data = json.loads(response.text)
