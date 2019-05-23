71e8f298b2f7f07eb70c459e1ed192fd6c38fea3
Oliver Walters <oliver.henry.walters@gmail.com>
2019-05-23 18:55:19 +1000

    Add a thing

--- tester.py
+++ tester.py
@@ -1,6 +1,15 @@
 from inventree_api import InvenTreeAPI
 from inventree_object import Part, BomItem
 
+""" User will need to provide a file called 'inventree_credentials.py'
+
+which contains:
+
+INVENTREE_URL e.g. 'http://127.0.0.1:8000/api/'
+INVENTREE_USERNAME e.g. 'my_username'
+INVENTREE_PASSWORD e.g. 'my_password'
+"""
+
 from inventree_credentials import INVENTREE_URL, INVENTREE_USERNAME, INVENTREE_PASSWORD
 
 api = InvenTreeAPI(INVENTREE_URL,
