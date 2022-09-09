7b1236e4898832ce5b8b23f157820b02017a4359
Unknown <martin@iggland.com>
2022-09-09 19:02:27 +0200

    Lint

--- test/test_label.py
+++ test/test_label.py
@@ -1,23 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import os
-# ~ import requests
 import sys
 
-# ~ from requests.exceptions import HTTPError
-
-# ~ try:
-    # ~ import Image
-# ~ except ImportError:
-    # ~ from PIL import Image
-
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-
 from test_api import InvenTreeTestCase  # noqa: E402
-
 from inventree.label import LabelPart, LabelStock, LabelLocation
 from inventree.part import Part
 from inventree.stock import StockItem, StockLocation
+
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 
 class LabelTest(InvenTreeTestCase):
@@ -35,10 +27,10 @@ class LabelTest(InvenTreeTestCase):
         # Make a new list filtered by LabelPart
         # List should be non-zero length
         # Filtered list should be equal the original list
-        lbl_part_list_filtered = [x for x in lbl_part_list if isinstance(x,LabelPart)]
+        lbl_part_list_filtered = [x for x in lbl_part_list if isinstance(x, LabelPart)]
 
         self.assertGreater(len(lbl_part_list_filtered), 0)
-        self.assertEqual(lbl_part_list,lbl_part_list_filtered)
+        self.assertEqual(lbl_part_list, lbl_part_list_filtered)
 
         # Stock Items
         # Get label list
@@ -47,10 +39,10 @@ class LabelTest(InvenTreeTestCase):
         # Make a new list filtered by LabelPart
         # List should be non-zero length
         # Filtered list should be equal the original list
-        lbl_stock_list_filtered = [x for x in lbl_stock_list if isinstance(x,LabelStock)]
+        lbl_stock_list_filtered = [x for x in lbl_stock_list if isinstance(x, LabelStock)]
 
         self.assertGreater(len(lbl_stock_list_filtered), 0)
-        self.assertEqual(lbl_stock_list,lbl_stock_list_filtered)
+        self.assertEqual(lbl_stock_list, lbl_stock_list_filtered)
 
         # Stock Locations
         # Get label list
@@ -59,10 +51,10 @@ class LabelTest(InvenTreeTestCase):
         # Make a new list filtered by LabelPart
         # List should be non-zero length
         # Filtered list should be equal the original list
-        lbl_location_list_filtered = [x for x in lbl_location_list if isinstance(x,LabelLocation)]
+        lbl_location_list_filtered = [x for x in lbl_location_list if isinstance(x, LabelLocation)]
 
         self.assertGreater(len(lbl_location_list_filtered), 0)
-        self.assertEqual(lbl_location_list,lbl_location_list_filtered)
+        self.assertEqual(lbl_location_list, lbl_location_list_filtered)
 
     def test_label_printing(self):
         """
@@ -79,9 +71,9 @@ class LabelTest(InvenTreeTestCase):
         lbl_part = LabelPart.list(self.api)[0]
 
         # Attempt to print to file - use label object
-        prt.printlabel(label=lbl_part, plugin=None, destination="partlabel_1.pdf");
+        prt.printlabel(label=lbl_part, plugin=None, destination="partlabel_1.pdf")
         # Attempt to print to file - use label ID directly
-        prt.printlabel(label=lbl_part.pk, plugin=None, destination="partlabel_2.pdf");
+        prt.printlabel(label=lbl_part.pk, plugin=None, destination="partlabel_2.pdf")
 
         # Make sure the files exist
         self.assertTrue(os.path.isfile("partlabel_1.pdf"))
@@ -95,9 +87,9 @@ class LabelTest(InvenTreeTestCase):
         lbl_sti = LabelStock.list(self.api)[0]
 
         # Attempt to print to file - use label object
-        sti.printlabel(label=lbl_sti, plugin=None, destination="stocklabel_1.pdf");
+        sti.printlabel(label=lbl_sti, plugin=None, destination="stocklabel_1.pdf")
         # Attempt to print to file - use label ID directly
-        sti.printlabel(label=lbl_sti.pk, plugin=None, destination="stocklabel_2.pdf");
+        sti.printlabel(label=lbl_sti.pk, plugin=None, destination="stocklabel_2.pdf")
 
         # Make sure the files exist
         self.assertTrue(os.path.isfile("stocklabel_1.pdf"))
@@ -111,9 +103,9 @@ class LabelTest(InvenTreeTestCase):
         lbl_sloc = LabelLocation.list(self.api)[0]
 
         # Attempt to print to file - use label object
-        sloc.printlabel(label=lbl_sloc, plugin=None, destination="locationlabel_1.pdf");
+        sloc.printlabel(label=lbl_sloc, plugin=None, destination="locationlabel_1.pdf")
         # Attempt to print to file - use label ID directly
-        sloc.printlabel(label=lbl_sloc.pk, plugin=None, destination="locationlabel_2.pdf");
+        sloc.printlabel(label=lbl_sloc.pk, plugin=None, destination="locationlabel_2.pdf")
 
         # Make sure the files exist
         self.assertTrue(os.path.isfile("locationlabel_1.pdf"))
