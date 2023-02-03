16db6b972c5e47b7b91d53ba61a3a0b6a9ac40d4
Oliver Walters <oliver.henry.walters@gmail.com>
2023-02-03 12:08:23 +1100

    Add some simple unit checks for currency conversion

--- test/test_currency.py
+++ test/test_currency.py
@@ -40,3 +40,31 @@ class CurrencyTest(InvenTreeTestCase):
 
         self.assertIsNotNone(rates)
         self.assertIsNotNone(mgr.base_currency)
+
+    def test_conversion(self):
+        """Test currency conversion"""
+
+        mgr = CurrencyManager(self.api)
+        mgr.updateFromServer()
+
+        # Override data
+        mgr.base_currency = 'USD'
+        mgr.exchange_rates = {
+            'USD': 1.00,
+            'AUD': 1.40,
+            'EUR': 0.90,
+            'JPY': 128
+        }
+
+        test_sets = [
+            (1, 'AUD', 'USD', 0.7143),
+            (1, 'EUR', 'USD', 1.1111),
+            (5, 'JPY', 'AUD', 0.0547),
+            (5, 'AUD', 'EUR', 3.2143),
+            (1, 'USD', 'JPY', 128)
+        ]
+
+        for value, source, target, result in test_sets:
+            converted = mgr.convertCurrency(value, source, target)
+
+            self.assertEqual(result, round(converted, 4))
