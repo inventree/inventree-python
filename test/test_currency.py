dc4d95a24e278a42d7bee703230627e0715f7627
Oliver Walters <oliver.henry.walters@gmail.com>
2023-02-03 11:36:48 +1100

    Unit testing for new currency manager clas

---
+++ test/test_currency.py
@@ -0,0 +1,42 @@
+"""Unit tests for currency exchange support"""
+
+
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+from test_api import InvenTreeTestCase  # noqa: E402
+
+from inventree.currency import CurrencyManager
+
+
+class CurrencyTest(InvenTreeTestCase):
+    """Tests for currency support"""
+
+    def test_fetch_data(self):
+        """Test that we can fetch currency data from the server"""
+
+        mgr = CurrencyManager(self.api)
+        mgr.updatFromServer()
+
+        self.assertIsNotNone(mgr.base_currency)
+        self.assertIsNotNone(mgr.exchange_rates)
+    
+    def test_base_currency(self):
+        """Test call to 'getBaseCurrency'"""
+
+        mgr = CurrencyManager(self.api)
+        base = mgr.getBaseCurrency()
+
+        self.assertEqual(base, 'USD')
+        self.assertIsNotNone(mgr.exchange_rates)
+    
+    def test_exchange_rates(self):
+        """Test call to 'getExchangeRates'"""
+
+        mgr = CurrencyManager(self.api)
+        rates = mgr.getExchangeRates()
+
+        self.assertIsNotNone(rates)
+        self.assertIsNotNone(mgr.base_currency)
