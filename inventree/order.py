35c0d939f68fa17907de71514a59fbcb9ebba94e
Oliver Walters <oliver.henry.walters@gmail.com>
2023-03-29 22:07:02 +1100

    PEP exceptions

--- inventree/order.py
+++ inventree/order.py
@@ -4,10 +4,10 @@ but the source for these models has now been moved into separate files
 """
 
 # Pass PurchaseOrder models through
-from inventree.purchase_order import PurchaseOrder, PurchaseOrderAttachment, PurchaseOrderExtraLineItem, PurchaseOrderLineItem
+from inventree.purchase_order import PurchaseOrder, PurchaseOrderAttachment, PurchaseOrderExtraLineItem, PurchaseOrderLineItem  # noqa:F401
 
 # Pass SalesOrder models through
-from inventree.sales_order import SalesOrder, SalesOrderAttachment, SalesOrderExtraLineItem, SalesOrderLineItem, SalesOrderShipment
+from inventree.sales_order import SalesOrder, SalesOrderAttachment, SalesOrderExtraLineItem, SalesOrderLineItem, SalesOrderShipment  # noqa:F401
 
 # Pass ReturnOrder models through
-from inventree.return_order import ReturnOrder, ReturnOrderAttachment, ReturnOrderExtraLineItem, ReturnOrderLineItem
+from inventree.return_order import ReturnOrder, ReturnOrderAttachment, ReturnOrderExtraLineItem, ReturnOrderLineItem  # noqa:F401
