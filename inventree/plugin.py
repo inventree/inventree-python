5a30242a7bd59068f8692401288339b0f0f86147
Oliver Walters <oliver.henry.walters@gmail.com>
2024-05-10 22:12:15 +1000

    Add basic class for Plugin introspection

---
+++ inventree/plugin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+import inventree.base
+
+class InvenTreePlugin:
+    """Represents a PluginConfig instance on the InvenTree server."""
+
+    URL = 'plugin'
