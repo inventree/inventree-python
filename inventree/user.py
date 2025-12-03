3010d20fadc496a58b187836ba2fd56f8ff137cf
Matt Campbell <matt@farixembedded.com>
2025-12-03 16:04:39 -0500

    Add Group and Owner api objects

--- inventree/user.py
+++ inventree/user.py
@@ -11,3 +11,15 @@ class User(inventree.base.InventreeObject):
     """ Class representing the User database model """
 
     URL = 'user/'
+
+
+class Group(inventree.base.InventreeObject):
+    """Class representing the Group database model"""
+
+    URL = "user/group/"
+
+
+class Owner(inventree.base.InventreeObject):
+    """Class representing the Owner database model"""
+
+    URL = "user/owner/"
